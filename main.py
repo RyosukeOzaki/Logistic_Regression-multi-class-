5ef8ac464bb98c50522c0f3d0b4ad70d46a25dfa
ozakiryosuke <38748341+RyosukeOzaki@users.noreply.github.com>
2018-07-03 01:52:25 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import os,sys
+import math
 from load import data_load
 from batch import batcher
 from model import *
-
-def evaluate(SR,input_x,label_y):
-    Loss = SR.cross_entropy_function(input_x,label_y)
-    Accuracy = SR.accuracy(input_x,label_y)
-    return Loss,Accuracy
 
 if __name__=='__main__':
     train_x,train_y,dev_x,dev_y,test_x,test_y = data_load()
@@ -15,16 +11,27 @@ if __name__=='__main__':
     epoch = 100
     n_in =  len(train_x[0])
     n_out = len(train_y[0])
-    learn_rate = 0.001
+    learn_rate = 0.01
+    m = math.ceil(len(train_y)/minibatch_size)
     #train
-    SR = Softmax_Regression(n_in,n_out,learn_rate,minibatch_size)
+    model = Softmax_Regression(n_in,n_out,learn_rate,minibatch_size)
     for ep in range(epoch):
+        loss = 0
+        accuracy = 0
         for input_x,label_y in batcher(train_x,train_y,minibatch_size):
-            Loss,Accuracy = evaluate(SR,input_x,label_y)
-            SR.update(input_x,label_y)
-            #print('Train | Epoch{0} | Data size{1} | Loss{2:.2f} | Accuracy{3:.2f}%'.format(ep+1,len(label_y),Loss,Accuracy*100))
-        Loss,Accuracy = evaluate(SR,dev_x,dev_y)
-        print('Dev | Epoch{0} | Data size{1} | Loss{2:.2f} | Accuracy{3:.2f}%'.format(ep+1,len(dev_y),Loss,Accuracy*100))
+            y = model.forward(input_x)
+            accuracy += model.accuracy(y,label_y)
+            loss += model.cross_entropy_function(y,label_y)
+            delta_w,delta_b = model.backward(input_x,y,label_y)
+            model.update(delta_w,delta_b)
+        print('Train | Epoch{0} | Data size{1} | Loss{2:.2f} | Accuracy{3:.2f}%'.format(ep+1,len(train_y),loss/m,accuracy/m*100))
+        if ep%10==0:
+            y = model.forward(dev_x)
+            accuracy = model.accuracy(y,dev_y)
+            loss = model.cross_entropy_function(y,dev_y)
+            print('Dev | Epoch{0} | Data size{1} | Loss{2:.2f} | Accuracy{3:.2f}%'.format(ep+1,len(dev_y),loss,accuracy*100))
     #test
-    Loss,Accuracy = evaluate(SR,input_x,label_y)
-    print('test | Data size{0} | Loss{1:.2f} | Accuracy{2:.2f}%'.format(len(dev_y),Loss,Accuracy*100))
+    y = model.forward(test_x)
+    accuracy = model.accuracy(y,test_y)
+    loss = model.cross_entropy_function(y,test_y)
+    print('Test | Data size{0} | Loss{1:.2f} | Accuracy{2:.2f}%'.format(len(test_y),loss,accuracy*100))
--- model.py
+++ model.py
@@ -3,41 +3,37 @@ import numpy as np
 
 class Softmax_Regression(object):
     def __init__(self, n_in, n_out, lr, minibatch_size):
-        self.W = np.zeros((n_out,n_in))
+        self.w = np.zeros((n_out,n_in))
         self.b = np.zeros(n_out)
         self.learn_rate = lr
         self.minibatch_size = minibatch_size
 
-    def linear(self,input_x):
-        return np.dot(input_x,self.W.T)+self.b
+    def linear(self,x):
+        return np.dot(x,self.w.T)+self.b
 
-    def softmax(self,input_x):
-        return np.exp(input_x)/np.sum(np.exp(input_x))
+    def softmax(self,x):
+        return np.exp(x)/np.sum(np.exp(x),axis=1,keepdims=True)
 
-    def forward(self,input_x):
-        pre_y = self.softmax(self.linear(input_x))
-        return np.argmax(pre_y,axis=1)
+    def forward(self,x):
+        pre_y = self.softmax(self.linear(x))
+        return pre_y
 
-    def backward(self,input_x,label_y):
-        pre_y = self.softmax(self.linear(input_x))
-        temp = (pre_y - label_y)/len(label_y)
+    def backward(self,input_x,pre_y,label_y):
+        temp = pre_y - label_y
         delta_w = np.dot(temp.T,input_x)
-        delta_b = np.sum(temp,axis=0)
+        delta_b = np.sum(temp,axis=0)/len(label_y)
         return delta_w,delta_b
 
-    def update(self,input_x,label_y):
-        delta_w,delta_b = self.backward(input_x,label_y)
-        self.W -= delta_w*self.learn_rate
+    def update(self,delta_w,delta_b):
+        self.w -= delta_w*self.learn_rate
         self.b -= delta_b*self.learn_rate
 
-
-    def cross_entropy_function(self,input_x,label_y):
+    def cross_entropy_function(self,pre_y,label_y):
         e=0.00001
-        pre_y = self.softmax(self.linear(input_x))
         cross_entropy_loss = -np.sum(label_y*np.log(pre_y+e))/len(label_y)
         return cross_entropy_loss
 
-    def accuracy(self,input_x,label_y):
-        pre_y = self.forward(input_x)
+    def accuracy(self,pre_y,label_y):
+        pre_y = np.argmax(pre_y,axis=1)
         y = np.argmax(label_y,axis=1)
         return np.sum(pre_y==y)/len(label_y)
